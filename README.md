dd267eb644210c015f146664d7027b16f29ac767
Joachim Hill-Grannec <joachim@suvault.com>
2017-06-12 02:40:28 -0400

    moved users and accesscode to the database and using PUT on rest endpoints to update as the dummy UI

--- README.md
+++ README.md
@@ -13,10 +13,51 @@ Project to create an automated butler for apartment buzzing systems to allow mul
 1. docker run classis/buzzerbutler
 2. set the following env variables
 
-TWILIO_ACCOUNT_SID
+TWILIO_ACCOUNT_SID - id from twilio
 
-TWILIO_TOKEN
+TWILIO_TOKEN - token from twilio
 
-TWILIO_LOCATION_NAME
+TWILIO_LOCATION_NAME - name that gets stated when called
 
-TWILIO_PHONE_NUMBER
+TWILIO_PHONE_NUMBER - twilio number that called id uses
+
+BB_DB_PATH - path to store state
+
+You must put into the users api end point PUT {host}/api/users an object that looks like
+```json
+[
+    {
+        "_id": "1",
+        "name": "joachim",
+        "code": "1",
+        "verb": "reach",
+        "phone": "+15555555555"
+    },
+    {
+        "_id": "2",
+        "name": "allie",
+        "code": "2",
+        "verb": "reach",
+        "phone": "+15555555555"
+    },
+    {
+        "_id": "3",
+        "name": "passcode",
+        "code": "3",
+        "verb": "use"
+    }
+]
+```
+and access codes PUT {host}/api/accesscodes an other that looks like
+```json
+[
+    {
+        "value": "1111",
+        "_id": 2
+    },
+    {
+        "value": "2222",
+        "_id": 3
+    }
+]
+```
--- server/server.js
+++ server/server.js
@@ -1,43 +1,85 @@
 import config from 'config';
 import express from 'express';
-import twilio from 'twilio';
-import body_parser from 'body-parser';
+import Twilio from 'twilio';
+import tingodb from 'tingodb';
+import bodyParser from 'body-parser';
 
-const urlencoded = body_parser.urlencoded;
-const VoiceResponse = twilio.twiml.VoiceResponse;
+const engine = tingodb();
+const dbPath = config.get('dbPath');
+const db = new engine.Db(dbPath, {});
+// const urlencoded = body_parser.urlencoded;
+const VoiceResponse = Twilio.twiml.VoiceResponse;
 const port = config.get('port');
-const twilio_sid = config.get('twilio.accountSid');
-const twilio_token = config.get('twilio.authToken');
-const location_name = config.get('locationName');
-const users = config.get('users');
-const passcodes = config.get('passcodes');
+const twilioSid = config.get('twilio.accountSid');
+const twilioToken = config.get('twilio.authToken');
+const locationName = config.get('locationName');
 const openTone = config.get('openTone');
 const phoneNumber = config.get('phoneNumber');
-
-const client = new twilio(twilio_sid, twilio_token);
+const accessCodesCol = db.collection('access_codes');
+const usersCol = db.collection('users');
+let users = [];
+let accessCodes = [];
+/* eslint-disable no-unused-vars */
+const client = new Twilio(twilioSid, twilioToken);
 const app = express();
 
 // Parse incoming POST params with Express middleware
-app.use(urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const generateIntro = () => {
   const options = users.map(user => `to ${user.verb} ${user.name} press ${user.code}`).join();
-  const whatToSay = `Welcome to ${location_name} ${options}`;
+  const whatToSay = `Welcome to ${locationName} ${options}`;
   console.log(whatToSay);
   return whatToSay;
 };
 
-app.post('/passcode' ,(req, res) => {
+app.put('/api/users', (req, res) => {
+  console.log(req.body);
+  usersCol.remove({});
+  users = req.body;
+  usersCol.insert(req.body, (err, result) => {
+    if (err) res.send(err);
+    res.send(result);
+  });
+});
+
+app.get('/api/users', (req, res) => {
+  usersCol.find({}).toArray((err, result) => {
+    if (err) res.send(err);
+    res.send(result);
+  });
+});
+
+app.put('/api/accesscodes', (req, res) => {
+  accessCodesCol.remove({});
+  accessCodes = req.body;
+  accessCodesCol.insert(req.body, (err, result) => {
+    if (err) res.send(err);
+    res.send(result);
+  });
+});
+
+app.get('/api/accesscodes', (req, res) => {
+  accessCodesCol.find({}).toArray((err, result) => {
+    if (err) res.send(err);
+    res.send(result);
+  });
+});
+
+
+// Passcode check
+app.post('/twilio/gatheraccesscode', (req, res) => {
   const twiml = new VoiceResponse();
-  if(req.body.Digits) {
+  if (req.body.Digits) {
     console.log(req.body.Digits);
-    if (passcodes.indexOf(req.body.Digits) > -1) {
-      twiml.say("Correct");
+    const exists = accessCodes.filter(object => (object.value === req.body.Digits));
+    if (exists.length !== 0) {
+      twiml.say('Correct');
       twiml.play({ digits: openTone });
     } else {
-      twiml.say("That is an incorrect passcode");
+      twiml.say('That is an incorrect accesscode');
     }
-
   } else {
     twiml.redirect('/twilio');
   }
@@ -45,26 +87,26 @@ app.post('/passcode' ,(req, res) => {
   res.send(twiml.toString());
 });
 
-app.post('/gather' ,(req, res) => {
+// First choice menu
+app.post('/twilio/gatherchoice', (req, res) => {
   console.log(req.body);
   const twiml = new VoiceResponse();
-  if(req.body.Digits) {
+  if (req.body.Digits) {
     console.log(req.body.Digits);
     // If the user entered digits, process their request
-
     const selected = users.filter(object => (object.code === req.body.Digits))
-      .map(object => {
-        if (object.verb === 'reach'){
-          twiml.dial({callerId: phoneNumber}, object.phone);
+      .map((object) => {
+        if (object.verb === 'reach') {
+          twiml.dial({ callerId: phoneNumber }, object.phone);
           console.log(twiml.toString());
           twiml.say(`${object.name} didnt answer`);
-        } else if(object.verb === 'use') {
-          const gather = twiml.gather({ numDigits: 4, action: '/passcode'});
+        } else if (object.verb === 'use') {
+          const gather = twiml.gather({ numDigits: 4, action: '/twilio/gatheraccesscode' });
           gather.say('Enter passcode');
         }
         return object;
       });
-    if(selected.length === 0){
+    if (selected.length === 0) {
       twiml.say('Sorry, I don\'t understand that choice.');
     }
   } else {
@@ -75,23 +117,27 @@ app.post('/gather' ,(req, res) => {
 });
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!');
 });
 
+
+// Starting point
 app.post('/twilio', (req, res) => {
-  const twiml = new VoiceResponse;
-  if (req.body.Digits) {
-    whatTodo(req.body.Digits, twiml);
-  } else {
-    const gather = twiml.gather({ numDigits: 1, action: '/gather' });
-    gather.say(generateIntro());
-    twiml.redirect('/twilio');
-  }
+  const twiml = new VoiceResponse();
+  const gather = twiml.gather({ numDigits: 1, action: '/twilio/gatherchoice' });
+  gather.say(generateIntro());
+  twiml.redirect('/twilio');
   res.type('text/xml');
   res.send(twiml.toString());
 });
 
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
+  accessCodesCol.find({}).toArray((err, result) => {
+    accessCodes = result;
+  });
+  usersCol.find({}).toArray((err, result) => {
+    users = result;
+  });
+  console.log(`Listening on port ${port}`);
 });
